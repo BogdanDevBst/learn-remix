915fe82717b70434fc53dbf2a53049d38b0635cc
Bogdan <bn.workdev@gmail.com>
2022-02-11 21:45:57 +0200

    add styles to project

--- app/root.tsx
+++ app/root.tsx
@@ -1,16 +1,20 @@
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration
-} from "remix";
-import type { MetaFunction } from "remix";
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from 'remix';
+import type { MetaFunction } from 'remix';
+import styles from './styles.css';
 
 export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
+  return { title: 'New Remix App' };
 };
+
+export function links() {
+  return [
+    {
+      rel: 'stylesheet',
+      href: 'https://unpkg.com/modern-normalize@1.1.0/modern-normalize.css',
+    },
+    { rel: 'stylesheet', href: styles },
+  ];
+}
 
 export default function App() {
   return (
@@ -25,7 +29,7 @@ export default function App() {
         <Outlet />
         <ScrollRestoration />
         <Scripts />
-        {process.env.NODE_ENV === "development" && <LiveReload />}
+        {process.env.NODE_ENV === 'development' && <LiveReload />}
       </body>
     </html>
   );
